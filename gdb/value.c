34101ec896545dd51a1b46caa58b8b0aab874a0e
wytrzeszcz <kuba@witomi.pl>
2018-09-13 15:42:14 +0200

    symbol info (cmd): add debug print array
    Change-Id: I96ce2e1f06c3674d6059d2e398280e3deeb28b2f

--- gdb/value.c
+++ gdb/value.c
@@ -2774,6 +2774,13 @@ unpack_long (struct type *type, const gdb_byte *valaddr)
          whether we want this to be true eventually.  */
       return extract_typed_address (valaddr, type);
 
+    case TYPE_CODE_ARRAY:
+      /* FIXME:I'm problably shoudyn't be prit here but somehow return array*/
+      printf("l:%d(bytes)\n",len);
+      for (int i=0;i<len;i++)
+          printf ("%02x ",valaddr[i]);
+      error (_("It's temporary print array."));
+
     default:
       error (_("Value can't be converted to integer."));
     }
